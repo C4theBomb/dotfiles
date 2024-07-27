cdb6592e3187a2c4930cff789983b0fd9d2a0668
C4 Patino <c4patino@gmail.com>
2024-07-27 18:44:29 -0500

    Added dotfiles to nixos configuration

--- .config/nvim/lua/configs/ufo.lua
+++
@@ -1,9 +0,0 @@
-require("ufo").setup({
-    provider_selector = function(bufnr, filetype, buftype)
-        return { 'treesitter', 'indent' }
-    end
-})
-
-local set = vim.keymap.set
-set("n", "zR", require("ufo").openAllFolds)
-set("n", "zM", require("ufo").closeAllFolds)
